a8fd193f9ab794cdfed76bb97754d3c7fe91c92a
jaanvi chirimar <92826861+jaanvic25@users.noreply.github.com>
2023-07-19 09:21:33 -0400

    Update findmissingnumbers.py

--- findmissingnumbers.py
+++ findmissingnumbers.py
@@ -51,7 +51,7 @@ class Solution:
             if len(numbers)==0:
                 return "Invalid input"
             else:
-                for i in range(int(numbers[0]), math.ceil(numbers[len(numbers)-1])):
+                for i in range(math.ceil(numbers[0]), math.ceil(numbers[len(numbers)-1])):
                     ctr = 0
                     for num in numbers:
                         if num % 1 != 0:
